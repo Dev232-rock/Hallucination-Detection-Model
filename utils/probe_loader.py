6f4584489be4b5e4a7ea5da55f5c14321a101c58
Dev232-rock <devdubey2002@gmail.com>
2026-05-12 00:14:25 +0530

    transformer layer from a model

--- utils/probe_loader.py
+++ utils/probe_loader.py
@@ -9,3 +9,6 @@ from typing import Optional, Tuple, Union
 import torch
 from huggingface_hub import HfApi, hf_hub_download, login
 from huggingface_hub.utils import validate_repo_id
+
+# Default directory for saving probes locally
+LOCAL_PROBES_DIR = Path(__file__).parent.parent / "value_head_probes"
