1e17ed123a90d59f5bbd5c1f1ac2ee256500a9e4
Dev232-rock <devdubey2002@gmail.com>
2026-06-01 23:55:55 +0530

    Upload a Probe

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -7,5 +7,7 @@ from rouge_score import rouge_scorer
 
 ROUGE_SCORER = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=False)
 def normalize_text(text: str) -> str:
-    """
-    Normalize text by removing control characters and standardizing Unicode.
+    # Normalize text by removing control characters and standardizing Unicode.
+
+    # Remove control characters except newline, tab, and carriage return
+    text = re.sub(r'[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]', '', text)
