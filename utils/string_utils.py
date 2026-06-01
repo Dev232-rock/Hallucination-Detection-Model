922e013d79ccd1f57438b6c81d307a3a5d14482b
Dev232-rock <devdubey2002@gmail.com>
2026-06-01 23:54:41 +0530

    Upload a Probe

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -4,3 +4,5 @@ from typing import Any, Dict, Type, Union, Optional, Tuple
 from pydantic import BaseModel, parse_obj_as
 from pydantic_core import from_json
 from rouge_score import rouge_scorer
+
+ROUGE_SCORER = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=False)
