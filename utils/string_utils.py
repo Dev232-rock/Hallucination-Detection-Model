a083d97669e1decab31ccf9661540fcfe97fcbf8
Dev232-rock <devdubey2002@gmail.com>
2026-06-03 23:56:13 +0530

    Upload a Probe

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -19,3 +19,6 @@ def normalize_text(text: str) -> str:
 
     # Replace Unicode quotes with standard quotes
     text = re.sub(r'[\u201C\u201D\u2018\u2019\u201E\u201F\u2039\u203A\u00AB\u00BB]', '"', text)
+
+    # Replace Unicode dashes with standard dash
+    text = re.sub(r'[\u2013\u2014\u2015]', '-', text)
