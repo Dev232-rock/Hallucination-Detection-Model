8366507c0b5fd8d16acf90ab832dbaf5b643b5ec
Dev232-rock <devdubey2002@gmail.com>
2026-07-29 10:54:22 +0530

    tokenization.py

--- utils/tokenization.py
+++ utils/tokenization.py
@@ -7,3 +7,14 @@ from transformers import AutoTokenizer
 def find_string_in_tokens(target: str, tokens: Tensor, tokenizer: AutoTokenizer, max_iters: int = 100) -> slice:
     # Performs a binary search to look for a target string inside some tokens.
     assert target in tokenizer.decode(tokens), "The target isn't contained in the whole array of tokens"
+    # Binary search over the end index of the slice
+    n_iters = max_iters
+    end_idx_left, end_idx_right = 0, len(tokens) 
+    while end_idx_left != end_idx_right and n_iters > 0:
+        mid = (end_idx_left + end_idx_right) // 2
+        if target in tokenizer.decode(tokens[:mid]):
+            end_idx_right = mid
+        else:
+            end_idx_left = mid + 1
+        n_iters -= 1
+    end_idx = end_idx_left
