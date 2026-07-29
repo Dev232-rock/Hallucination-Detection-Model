75e05025a25a6d13b939d660e2fcf97adefdc65a
Dev232-rock <devdubey2002@gmail.com>
2026-07-29 10:53:50 +0530

    tokenization.py

--- utils/tokenization.py
+++ utils/tokenization.py
@@ -4,3 +4,6 @@ import torch
 from jaxtyping import Int
 from torch import Tensor
 from transformers import AutoTokenizer
+def find_string_in_tokens(target: str, tokens: Tensor, tokenizer: AutoTokenizer, max_iters: int = 100) -> slice:
+    # Performs a binary search to look for a target string inside some tokens.
+    assert target in tokenizer.decode(tokens), "The target isn't contained in the whole array of tokens"
