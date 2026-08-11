84fcfe6135b5d16dc56b9c9a83d52a35c8b5df6c
Dev232-rock <devdubey2002@gmail.com>
2026-08-12 00:40:58 +0530

    config.py

--- probe/config.py
+++ probe/config.py
@@ -7,3 +7,8 @@ from typing import List, Optional, Union, Literal
 from utils.probe_loader import LOCAL_PROBES_DIR
 from utils.model_utils import get_num_layers
 from .dataset import TokenizedProbingDatasetConfig
+
+@dataclass
+class ProbeConfig:
+    # Configuration for a probe model.
+    probe_id: str = "llama3_1_8b_lora_lambda_kl=0.5"
