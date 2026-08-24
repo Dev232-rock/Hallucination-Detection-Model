e1595a7d1ad8b85aa2cf0999fc42a00a5e8dc052
Dev232-rock <devdubey2002@gmail.com>
2026-08-25 00:33:20 +0530

    config.py

--- probe/config.py
+++ probe/config.py
@@ -101,3 +101,7 @@ class TrainingConfig:
         # Post-initialization processing.
         if isinstance(self.probe_config, dict):
             self.probe_config = ProbeConfig(**self.probe_config)
+
+          # Handle special values
+        if self.eval_steps == -1:
+            self.eval_steps = None
